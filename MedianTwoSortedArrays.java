5494d70d25e7e0246ae79c649a88b0a29844349d
Kennedy Odhiambo <x.johnkennedy.x@gmail.com>
2024-06-24 10:54:20 +0300

    finalise MedianTwoSortedArrays

--- MedianTwoSortedArrays.java
+++ MedianTwoSortedArrays.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 public class MedianTwoSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
         // Merge nums1 and nums2 into a single sorted array nums3
         int[] nums3 = new int[nums1.length + nums2.length];
         System.arraycopy(nums1, 0, nums3, 0, nums1.length);
         System.arraycopy(nums2, 0, nums3, nums1.length, nums2.length);
+        Arrays.sort(nums3);
 
         // Determine the length of nums3 and whether it's even or odd
         int nums3Length = nums3.length;
@@ -14,6 +18,7 @@ public class MedianTwoSortedArrays {
             // If nums3 has even length, average of middle two elements
             int secondMiddleIndex = nums3Length / 2;
             int firstMiddleIndex = secondMiddleIndex - 1;
+
             return (nums3[firstMiddleIndex] + nums3[secondMiddleIndex]) / 2.0;
         } else {
             // If nums3 has odd length, middle element
